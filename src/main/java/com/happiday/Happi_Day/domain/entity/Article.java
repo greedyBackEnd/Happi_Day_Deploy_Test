7b66d5848ba97a5da30e559f5fa4e8e4cedd56af
sumin <csumin008255@gmail.com>
2023-07-25 01:52:13 +0900

    feat: Article Entity 작성

---
+++ src/main/java/com/happiday/Happi_Day/domain/entity/Article.java
@@ -0,0 +1,57 @@
+package com.happiday.Happi_Day.domain.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@SuperBuilder(toBuilder = true)
+@Table(name = "article")
+public class Article extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // 게시판 id
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name="board_id")
+//    private Board board;
+
+    // 유저 id
+//    @ManyToOne(fetch =FetchType.LAZY)
+//    @JoinColumn(name = "user_id")
+//    private User user;
+
+    @NotNull
+    private String title;
+
+    @NotNull
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    // 댓글 매핑
+//    @NotNull
+//    @OneToMany(mappedBy="comment")
+//    private List<Comment> comments = new ArrayList<>();
+
+    // 게시글 좋아요 매핑
+//    @NotNull
+//    @OneToMany(mappedBy="user_article_likes")
+//    private List<UserArticleLikes> userArticleLikes = new ArrayList<>();
+
+    // 게시글 스크랩 매핑
+//    @NotNull
+//    @OneToMany(mappedBy="user_article_scrap")
+//    private List<UserArticleScrap> userArticleScraps = new ArrayList<>();
+
+}
