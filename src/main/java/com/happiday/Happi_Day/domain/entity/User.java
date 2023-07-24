566bcd7a73706fea9525cbb4c926c39b012ea447
Minwookim <trte135@naver.com>
2023-07-25 01:00:38 +0900

    [Feature] #9 UserEntity 수정
    - UserClass : 
          1. 유저 역할구분 타입을 Role 에서 String 타입으로 수정
          2. 각 Column명 수정
          3. Date type을 LocalDateTime으로 수정
          4. @CreatedDate 애노테이션을 이용하여 회원가입 날짜를 생성 되도록 수정, @LastModifiedDate 애노테이션을 이용하여 회원정보 수정날짜가 생성되도록 수정
          5. boolean 타입을 Boolean 타입으로 수정(isActive, isDeleted)
          6. lastLoginAt 컬럼 타입도 Date type 에서 LocalDateTime 타입으로 수정
          7. Role enum Class 삭제 => UserEntity Class에 Role type 을 String 타입으로 수정

--- src/main/java/com/happiday/Happi_Day/domain/entity/User.java
+++ src/main/java/com/happiday/Happi_Day/domain/entity/User.java
@@ -1,53 +1,56 @@
 package com.happiday.Happi_Day.domain.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.experimental.SuperBuilder;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.format.annotation.DateTimeFormat;
 
-import java.sql.Date;
+import java.time.LocalDateTime;
 
+@Data
 @Getter
 @Entity
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@SuperBuilder(toBuilder = true)
 @Table(name = "users")
-public class User extends BaseEntity {
+@NoArgsConstructor
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // 유저 식별 ID
 
     // 제약사항 추가
-    @Column(name = "email", nullable = false, unique = true)
-    private String email; // 이메일형식(중복 X)
-    @Column(name = "password", nullable = false)
+    @Column(nullable = false, unique = true)
+    private String userEmail; // 이메일형식(중복 X)
+
+    @Column(nullable = false)
     private String password; // 비밀번호
 
-    @Column(name = "nickname", nullable = false, unique = true)
+    @Column(nullable = false, unique = true)
     private String nickName; // 닉네임(중복 X)
-    @Column(name = "username", nullable = false)
+    @Column(nullable = false)
     private String userName; // 실명
 
-    @Column(name = "phone", nullable = false, unique = true)
-    private String phone; // 전화번호(중복 X)
+    @Column(nullable = false, unique = true)
+    private String phoneNumber; // 전화번호(중복 X)
 
-    @Column(name = "created_at", nullable = false)
-    private Date createdAt; // 회원가입 날짜
-    @Column(name = "update_at", nullable = false)
-    private Date updateAt; // 회원정보 수정 날짜
+    @Column(nullable = false)
+    @CreatedDate
+    private LocalDateTime createdAt; // 회원가입 날짜
 
-    @Column(name = "role", nullable = false, columnDefinition = "user")
-    private Role role; // 회원구분(default => user[value])
+    @Column(nullable = false)
+    @LastModifiedDate
+    private LocalDateTime updateAt; // 회원정보 수정 날짜
 
-    @Column(name = "is_active", nullable = false, columnDefinition = "true")
-    private boolean isActive; // 활성화 상태구분(default => true)
+    @Column(nullable = false)
+    private String role; // 회원구분
 
-    @Column(name = "is_deleted", nullable = false, columnDefinition = "false")
-    private boolean isDeleted; // 탈퇴, 관리자에 의한 삭제 구분(default => false)
+    @Column(nullable = false)
+    private Boolean isActive; // 활성화 상태구분(default => true)
 
-    @Column(name = "last_login_at", nullable = true)
-    private Date lastLoginAt; // 마지막 로그인 날짜
+    @Column(nullable = false)
+    private Boolean isDeleted; // 탈퇴, 관리자에 의한 삭제 구분(default => false)
+
+    private LocalDateTime lastLoginAt; // 마지막 로그인 날짜
 }
