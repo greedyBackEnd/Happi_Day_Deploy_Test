9e9d9d6251bd6bb44bc77e01b72fb550dbd30e78
Minwookim <trte135@naver.com>
2023-07-25 14:38:15 +0900

    [Feature] #9 UserEntity 매핑작업[완료]
    1.  다대다 매핑 구조 TODO 주석으로 각 필요한 매핑작업 완료
    2. createdAt, updateAt 컬럼은 BaseEntity 에서 가져오도록 수정
    3. 회원구분에서 Role Enum Class를 생성하여 User, Admin 인지 구분할 수 있게 수정

---
+++ src/main/java/com/happiday/Happi_Day/domain/entity/RoleType.java
@@ -0,0 +1,6 @@
+package com.happiday.Happi_Day.domain.entity;
+
+public enum RoleType {
+    USER, // 회원
+    ADMIN; // 슈퍼 관리자
+}
--- src/main/java/com/happiday/Happi_Day/domain/entity/User.java
+++ src/main/java/com/happiday/Happi_Day/domain/entity/User.java
@@ -1,12 +1,7 @@
 package com.happiday.Happi_Day.domain.entity;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.annotations.CreationTimestamp;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -14,7 +9,8 @@ import java.time.LocalDateTime;
 @Getter
 @Entity
 @Table(name = "users")
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+// 유저
 public class User extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,8 +31,9 @@ public class User extends BaseEntity {
     @Column(nullable = false, unique = true)
     private String phoneNumber; // 전화번호(중복 X)
 
+    @Enumerated(EnumType.STRING)
     @Column(nullable = false)
-    private String role; // 회원구분
+    private RoleType role; // 회원구분
 
     @Column(nullable = false)
     private Boolean isActive; // 활성화 상태구분(default => true)
@@ -45,4 +42,164 @@ public class User extends BaseEntity {
     private Boolean isDeleted; // 탈퇴, 관리자에 의한 삭제 구분(default => false)
 
     private LocalDateTime lastLoginAt; // 마지막 로그인 날짜
+
+    // TODO 주문 매핑
+    /*
+         @ManyToMany
+         @JoinTable(
+            name = "order",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id"))
+         private List<Order> order = new ArrayList<>();
+    */
+
+    // TODO 장바구니 매핑
+    /*
+         @ManyToMany
+         @JoinTable(
+            name = "cart",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id"))
+         private List<Cart> cart = new ArrayList<>();
+    */
+
+    // TODO 상품 매핑
+    /*
+         @ManyToMany
+         @JoinTable(
+            name = "product",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+         private List<Product> product = new ArrayList<>();
+    */
+
+    // TODO 상품 찜하기 매핑
+    /*
+         @ManyToMany
+         @JoinTable(
+            name = "user_product_likes",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "product_id"))
+         private List<Product> product = new ArrayList<>();
+    */
+
+    // TODO 게시글 매핑
+    /*
+         @ManyToMany
+         @JoinTable(
+            name = "article",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "board_id"))
+         private List<Article> article = new ArrayList<>();
+    */
+
+    // TODO 댓글 매핑
+    /*
+         @ManyToMany
+         @JoinTable(
+            name = "comment",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "article_id"))
+         private List<Comment> comment = new ArrayList<>();
+    */
+
+    // TODO 게시글 좋아요 매핑
+    /*
+         @ManyToMany
+         @JoinTable(
+            name = "user_article_likes",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "article_id"))
+         private List<Article> article = new ArrayList<>();
+    */
+
+    // TODO 게시글 스크랩 매핑
+    /*
+         @ManyToMany
+         @JoinTable(
+            name = "user_article_scrap",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "article_id"))
+         private List<Article> article = new ArrayList<>();
+    */
+
+    // TODO 유저-아티스트 구독 매핑
+    /*
+         @ManyToMany
+         @JoinTable(
+            name = "user_article_subscription",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "article_id"))
+         private List<Article> article = new ArrayList<>();
+    */
+
+    // TODO 유저-팀 구독 매핑
+    /*
+         @ManyToMany
+         @JoinTable(
+            name = "user_team_subscription",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "team_id"))
+         private List<Team> team = new ArrayList<>();
+    */
+
+    // TODO 이벤트 매핑
+    /*
+         @ManyToMany
+         @JoinTable(
+            name = "event",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "event_id"))
+         private List<Event> Event = new ArrayList<>();
+    */
+
+    // TODO 이벤트 댓글 매핑
+    /*
+         @ManyToMany
+         @JoinTable(
+            name = "event_comment",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "event_id"))
+         private List<Event> Event = new ArrayList<>();
+    */
+
+    // TODO 유저-이벤트 참가하기 매핑
+    /*
+         @ManyToMany
+         @JoinTable(
+            name = "user_event_participation",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "event_id"))
+         private List<Event> Event = new ArrayList<>();
+    */
+
+    // TODO 유저-이벤트 참가하기 매핑
+    /*
+         @ManyToMany
+         @JoinTable(
+            name = "user_event_likes",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "event_id"))
+         private List<Event> Event = new ArrayList<>();
+    */
+
+    // TODO 채팅방 매핑
+    /*
+         @ManyToMany
+         @JoinTable(
+            name = "chat_room",
+            joinColumns = @JoinColumn(name = "sender_id"),
+            inverseJoinColumns = @JoinColumn(name = "receiver_id"))
+         private List<ChatRoom> chatRoom = new ArrayList<>();
+    */
+
+    // TODO 채팅메세지 매핑
+    /*
+         @ManyToMany
+         @JoinTable(
+            name = "chat_message",
+            joinColumns = @JoinColumn(name = "sender_id"),
+            inverseJoinColumns = @JoinColumn(name = "chat_room_id"))
+         private List<ChatRoom> chatRoom = new ArrayList<>();
+    */
 }
