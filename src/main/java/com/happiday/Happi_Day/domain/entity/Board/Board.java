674404147996c99045cc5863e9335ee99e905630
sumin <csumin008255@gmail.com>
2023-07-27 13:24:48 +0900

    refactor: Board 패키지 생성   - Board, BoardCategory Entity 파일 이동

--- src/main/java/com/happiday/Happi_Day/domain/entity/Board/Board.java
+++ src/main/java/com/happiday/Happi_Day/domain/entity/Board/Board.java
@@ -1,5 +1,6 @@
-package com.happiday.Happi_Day.domain.entity;
+package com.happiday.Happi_Day.domain.entity.Board;
 
+import com.happiday.Happi_Day.domain.entity.BaseEntity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
@@ -7,9 +8,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
 
 @Entity
 @Getter
