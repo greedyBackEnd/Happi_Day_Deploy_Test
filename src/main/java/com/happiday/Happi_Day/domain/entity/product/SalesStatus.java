ce8cb8121da5152451f3656a8dc3dd2863017b9b
sumin <csumin008255@gmail.com>
2023-07-28 00:24:57 +0900

    refactor: ProductStatus -> SalesStatus 이름 변경

--- src/main/java/com/happiday/Happi_Day/domain/entity/product/SalesStatus.java
+++ src/main/java/com/happiday/Happi_Day/domain/entity/product/SalesStatus.java
@@ -3,14 +3,14 @@ package com.happiday.Happi_Day.domain.entity.product;
 import lombok.Getter;
 
 @Getter
-public enum ProductStatus {
+public enum SalesStatus {
     ON_SALE("판매중"),
     STOP_SALE("판매중지"),
     OUT_OF_STOCK("품절");
 
     private String value;
 
-    ProductStatus(String value){
+    SalesStatus(String value){
         this.value = value;
     }
 
