9551a7943ab9ea9a929b9f6577075c41f87548d0
2hapu <yonzn@naver.com>
2023-10-06 20:33:30 +0900

    feat: 불필요한 주석 제거

--- src/main/java/com/happiday/Happi_Day/domain/repository/EventRepository.java
+++ src/main/java/com/happiday/Happi_Day/domain/repository/EventRepository.java
@@ -3,7 +3,5 @@ package com.happiday.Happi_Day.domain.repository;
 import com.happiday.Happi_Day.domain.entity.event.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface EventRepository extends JpaRepository<Event, Long> {
 }
